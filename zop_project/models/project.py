85e3b2630098ae6e14441631359a1d0f39e1c786
winboy99 <odooht@163.com>
2019-01-08 21:00:07 +0800

    project daywork fullname

--- zop_project/models/project.py
+++ zop_project/models/project.py
@@ -105,7 +105,7 @@ class TaskDaywork(models.Model):
     @api.depends('project_id','task_id.full_name')
     def _compute_name(self):
         for rec in self:
-            rec.name  = rec.task_id.name + fields.Date.to_string(rec.date)
+            rec.name  = rec.task_id.name + '.' + fields.Date.to_string(rec.date)
             rec.full_name = rec.task_id.full_name + '.' + fields.Date.to_string(rec.date)
 
 
